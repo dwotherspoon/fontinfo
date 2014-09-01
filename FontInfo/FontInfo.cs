5abe00d9f6b9afa359797eabe539cacc3d5dc0cf
David Wotherspoon <davidwother@hotmail.co.uk>
2014-09-01 07:25:35 +0100

    Write some stubs, do some tests.

---
+++ FontInfo/FontInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace FontInfo
+{
+    public class FontInfo
+    {
+        public enum fontType { TTF, OTF, FON, TTC, PFB, PFA, ERR};
+        private fontType type = fontType.ERR;
+        public fontType Type
+        {
+            get { return type; }
+        }
+
+        private BinaryReader reader;
+
+        public FontInfo(string file)
+        {
+            if (!File.Exists(file)) throw new FileNotFoundException();
+            this.reader = new BinaryReader(new FileStream(file, FileMode.Open, FileAccess.Read));
+            byte[] magic_num = reader.ReadBytes(4);
+            Console.WriteLine(magic_num[0] + ", " + magic_num[1] + ", " + magic_num[2] + ", " + magic_num[3]);
+            if (magic_num.Intersect(new byte[] {0x00, 0x01, 0x00, 0x00}).Count() == 4)
+            {
+                type = fontType.TTF;
+            }
+            else if (magic_num.Intersect(new byte[] {0x4F, 0x54, 0x54, 0x4F}).Count() == 4)
+            {
+                type = fontType.OTF;
+            }
+        }
+
+
+    }
+}
---
+++ FontInfo/FontInfoTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FontInfo
+{
+    [TestFixture]
+    class FontInfoTest
+    {
+        [SetUp]
+        public void Init()
+        {
+            //
+        }
+
+        [Test]
+        public void Test1()
+        {
+            FontInfo fi = new FontInfo("Not a file");
+            Assert.NotNull(fi);
+        }
+
+        [Test]
+        public void TestTTF()
+        {
+            FontInfo fi = new FontInfo(@"C:\Windows\Fonts\times.ttf");
+            FontInfo fi2 = new FontInfo(@"C:\Windows\Fonts\MyriadPro-Bold.otf");
+            Assert.AreEqual(fi.Type, FontInfo.fontType.TTF);
+            Assert.AreEqual(fi2.Type, FontInfo.fontType.OTF);
+        }
+    }
+}
